3b2ad3d321ce66e77652d6d5d3bae9ac8373527c
Robson <robsonribeirorra@gmail.com>
2023-12-16 10:50:05 -0300

    UserService added

--- src/main/java/br/com/projeto/template/service/UserService.java
+++ src/main/java/br/com/projeto/template/service/UserService.java
@@ -1,5 +1,41 @@
 package br.com.projeto.template.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projeto.template.dto.UserDto;
+import br.com.projeto.template.entity.UserEntity;
+import br.com.projeto.template.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<UserDto> listAll() {
+		List<UserEntity> users = userRepository.findAll();
+		return users.stream().map(UserDto::new).toList();
+	}
+	
+	public void createUser(UserDto user) {
+		UserEntity userEntity = new UserEntity(user);
+		userRepository.save(userEntity);
+	}
+	
+	public UserDto updateUser(UserDto user) {
+		UserEntity userEntity = new UserEntity(user);
+		return new UserDto(userRepository.save(userEntity));
+	}
+	
+	public void deleteUser(Long id) {
+		UserEntity userEntity = userRepository.findById(id).get();
+		userRepository.delete(userEntity);
+	}
+	
+	public UserDto searchById(Long id) {
+		return new UserDto(userRepository.findById(id).get());
+	}
 }
